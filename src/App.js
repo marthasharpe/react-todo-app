e005e3a713d17865b1c32cfc2b8589a61b314766
Martha Sharpe <marthasharpe2020@gmail.com>
2019-09-04 15:24:53 -0400

    finish react todo list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Todos from './Todos';
 import './App.css';
+import AddTodo from './AddTodo';
 
 class App extends Component {
   state = {
@@ -17,11 +18,19 @@ class App extends Component {
       todos
     })
   }
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    })
+  }
   render() {
     return (
       <div className="app-container">
         <h1 id="title">Todos</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     );
   }
